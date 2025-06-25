d7d9bd21a7fda39a881da87233247ccce95d1a4c
anubhavdwivedy <anubhavdwivedy@gmail.com>
2025-06-25 20:11:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, login_required, logout_user, UserMixin, current_user
 from rembg import remove
