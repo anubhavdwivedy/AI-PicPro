d4ff8f091f2c5f6859d1c49e08404654860ec4a5
anubhavdwivedy <anubhavdwivedy@gmail.com>
2025-06-26 11:44:54 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from app import app, db
-import os
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-        os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-        os.makedirs(app.config['PROCESSED_FOLDER'], exist_ok=True)
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
