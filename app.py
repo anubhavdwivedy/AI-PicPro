fb801e115019bfe0e2d9ec52bc07f051d9d51433
anubhavdwivedy <anubhavdwivedy@gmail.com>
2025-06-26 19:22:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -167,3 +167,7 @@ def chat():
 # -------------------- DB Init --------------------
 with app.app_context():
     db.create_all()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
